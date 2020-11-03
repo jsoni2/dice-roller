fab86b2afed7edcb6ce96a3e69ad5976f88d74f9
Janak Soni <Janak.X.Soni@kp.org>
2020-11-03 09:24:45 -0600

    Replace reference to the TextView with the ImageView

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
@@ -20,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
         val randomInt = Random().nextInt(6) + 1
         resultText.text = randomInt.toString()
     }
